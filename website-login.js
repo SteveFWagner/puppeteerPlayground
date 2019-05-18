f6040c70de7d3478a6e3249f6ea24c54204a0f8a
Rendstein <sfwagner1011@gmail.com>
2019-05-18 09:58:07 -0600

    more fun

--- website-login.js
+++ website-login.js
@@ -1,48 +1,51 @@
+require('dotenv').config()
 const puppeteer = require('puppeteer');
 
 (async () => {
   const browser = await puppeteer.launch({headless:false});
   const page = await browser.newPage();
 
-    
+  const loginButton = '#root > div > div:nth-child(1) > header > div > div:nth-child(5) > button'
+  const emailInput = 'body > div.jss74 > main > div > form > div:nth-child(1) > div > input'
+  const passInput = 'body > div.jss74 > main > div > form > div:nth-child(2) > div > input'
+  const submitLoginButton = 'body > div.jss74 > main > div > form > button:nth-child(3)'
+  const accountButton = '#root > div > div:nth-child(1) > header > div > div.jss112'
+  const accountBio = '#profilepaper > p:nth-child(4)'
+  const {THISISTHEUSEREMAIL,THEPASS} = process.env    
 
   await page.goto('http://u-knight.ninja',{waitUntil: 'networkidle2'});
-  await page.click('#root > div > div:nth-child(1) > header > div > div:nth-child(5) > button')
+  await page.click(loginButton)
 
 //wait for and input user email
-  await page.waitForSelector('body > div.jss74 > main > div > form > div:nth-child(1) > div > input')
-  await page.focus('body > div.jss74 > main > div > form > div:nth-child(1) > div > input')
-  await page.keyboard.type('steve.Testing@gmail.com')
+  await page.waitForSelector(emailInput)
+  await page.focus(emailInput)
+  await page.keyboard.type(THISISTHEUSEREMAIL)
 
 //wait for and input user password
-  await page.waitForSelector('body > div.jss74 > main > div > form > div:nth-child(2) > div > input')
-  await page.focus('body > div.jss74 > main > div > form > div:nth-child(2) > div > input')
-  await page.keyboard.type('!Testing12345')
+  await page.waitForSelector(passInput)
+  await page.focus(passInput)
+  await page.keyboard.type(THEPASS)
 
-//login
-  await page.click('body > div.jss74 > main > div > form > button:nth-child(3)')
+//submit login
+  await page.click(submitLoginButton)
 
 //navigation to account page
   await page.waitFor(2000)
-  await page.waitForSelector('#root > div > div:nth-child(1) > header > div > div.jss112')
-  await page.click('#root > div > div:nth-child(1) > header > div > div.jss112')
+  await page.waitForSelector(accountButton)
+  await page.click(accountButton)
 
-//wait for and assign biography to const
-  await page.waitForSelector('#profilepaper > p:nth-child(4)')
-
+//wait for biography to load, assign it to a const and console log it
+  await page.waitForSelector(accountBio)
     function waitForBio () {
         return document.querySelector('#profilepaper > p:nth-child(4)').innerHTML != ''
     }
-
   await page.waitFor(waitForBio)
-
-  const biography = await page.evaluate(()=>{
-    return document.querySelector('#profilepaper > p:nth-child(4)').innerHTML
-  })
+    const biography = await page.evaluate(()=>{
+        return document.querySelector('#profilepaper > p:nth-child(4)').innerHTML
+    })
   console.log(biography)
 
   await page.waitFor(5000)
-
   await page.screenshot({path: 'u-knight.png'});
   await browser.close();
 })();
